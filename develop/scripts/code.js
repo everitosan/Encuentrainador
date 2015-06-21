39e612b987703cb4baa03851959deb9bdbebc9b8
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-06-21 09:51:25 -0500

    (feat): dependencies injection

--- develop/scripts/code.js
+++ develop/scripts/code.js
@@ -1,6 +1,8 @@
 (function(){
 	angular.module('hackAManoApp',  [
 		'ui.bootstrap',
+		'googleplus',
+		'facebook',
     	'ngRoute',
 		'hackAManoApp.directives',
 		'hackAManoApp.controllers',
@@ -10,4 +12,16 @@
 	angular.module('hackAManoApp.controllers', []); // Notice the empty array at the end here
 	angular.module('hackAManoApp.directives', []); // Notice the empty array at the end here
 	angular.module('hackAManoApp.services', []); // Notice the empty array at the end here
+
+	angular.module('hackAManoApp')
+	.config(['$routeProvider', function($routeProvider){
+			$routeProvider
+				.when("/",{
+					templateUrl: 'templates/homeModule/tmpl.html'
+				})
+				.when("/searchs", {
+					templateUrl: 'templates/homeInModule/tmpl.html'
+				});
+		}]);
+
 })();
