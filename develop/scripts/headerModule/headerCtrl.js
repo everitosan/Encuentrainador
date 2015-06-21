86fdc7e7bae3ed32d729978faf771cc6c5e0aa0f
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-06-21 09:52:05 -0500

    (feat): Log with Facebook

--- develop/scripts/headerModule/headerCtrl.js
+++ develop/scripts/headerModule/headerCtrl.js
@@ -1,22 +1,26 @@
 angular.module('hackAManoApp.controllers',[])
-	.controller('headerCtrl', ['$scope', '$modal', function($scope, $modal){
+	.controller('headerCtrl', ['$scope', '$modal','$rootScope' , function($scope, $modal, $rootScope){
+		$scope.open = false;
 
 		$scope.animationsEnabled = true;
 		
-		$scope.logIn = function(){
+		$scope.logIn = function(type){
 
+			$rootScope.modalType = type;
 			var modalIntsance = $modal.open({
 		      templateUrl: 'templates/LogInModule/modalTmpl.html',
 		      controller: 'LogInCtrl',
 		      size: 'lg'
 		    });
-		};
 
-		$scope.signUp = function () {
-			var modalIntsance = $modal.open({
-		      templateUrl: 'templates/signUpModule/modalTmpl.html',
-		      controller: 'signUpCtrl',
-		      size: 'lg'
+
+		    modalIntsance.result.then(
+		    	function(){
+		    		$scope.open = true;
+		    },  function(){
+		    		$scope.open = false;
 		    });
 		};
+
+	
 	}]);
